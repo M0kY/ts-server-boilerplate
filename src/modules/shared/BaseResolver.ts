90b6d3d634602589386122436ac5ecac2e5762ac
M0kY <46133205+M0kY@users.noreply.github.com>
2019-07-21 03:27:34 +0200

    Add base resolver to extend others and apply/reuse CRUD operations

---
+++ src/modules/shared/BaseResolver.ts
@@ -0,0 +1,23 @@
+import { ClassType, Resolver, Query, Arg, Mutation } from 'type-graphql';
+import { BaseEntity } from 'typeorm';
+
+export function createBaseResolver<
+  T extends ClassType,
+  X extends ClassType,
+  I extends typeof BaseEntity & { new (...args: any[]): any }
+>(suffix: string, inputType: T, returnType: X, entity: I) {
+  @Resolver({ isAbstract: true })
+  abstract class BaseResolver {
+    @Query(() => [returnType], { name: `getAll${suffix}` })
+    async getAll(): Promise<T[] | undefined | []> {
+      return await entity.find();
+    }
+
+    @Mutation(() => returnType, { name: `create${suffix}` })
+    async create(@Arg('data', () => inputType) data: any): Promise<T> {
+      return await entity.create(data).save();
+    }
+  }
+
+  return BaseResolver;
+}
