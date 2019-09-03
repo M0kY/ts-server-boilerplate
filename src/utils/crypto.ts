4c93734f5c166b9145f2aec879c50b0fc9edfa35
M0kY <46133205+M0kY@users.noreply.github.com>
2019-09-03 23:16:29 +0200

    Password hashing method change

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,10 +1,19 @@
 import bcrypt from 'bcrypt';
-import { BCRYPT_SALT_ROUNDS } from '../config/envConfig';
+import { createHmac } from 'crypto';
+import { BCRYPT_SALT_ROUNDS, PASSWORD_HMAC_SECRET } from '../config/envConfig';
+
+const generateHmacSha256Hash = (password: string, secret: string = PASSWORD_HMAC_SECRET) => {
+  return createHmac('sha256', secret)
+    .update(password)
+    .digest('base64');
+};
 
 export const hashPassword = (password: string): string => {
-  return bcrypt.hashSync(password, BCRYPT_SALT_ROUNDS);
+  const hmacPasswordHash = generateHmacSha256Hash(password);
+  return bcrypt.hashSync(hmacPasswordHash, BCRYPT_SALT_ROUNDS);
 };
 
 export const comparePasswords = (password: string, hashedPassword: string): boolean => {
-  return bcrypt.compareSync(password, hashedPassword);
+  const hmacPasswordHash = generateHmacSha256Hash(password);
+  return bcrypt.compareSync(hmacPasswordHash, hashedPassword);
 };
